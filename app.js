52571d9f567243dc4bdd7865b6dbc891d2bdef5e
Sadhna <singhprits138@gmail.com>
2019-10-08 23:31:01 +0530

    First version of this

---
+++ app.js
@@ -0,0 +1,33 @@
+class NameField {
+    constructor(name) {
+        const field = document.createElement('li');
+        field.textContent = name;
+        const nameListHook = document.querySelector('#names');
+        nameListHook.appendChild(field);
+    }
+}
+
+class NameGenerator {
+    constructor() {
+        const btn = document.querySelector('button');
+        this.names = ["Max","Manu","Sahil","Siddhu"]
+        this.currentIndex =0;
+       btn.addEventListener('click', this.addName.bind(this)); // alternative
+        // btn.addEventListener('click', () => {
+        //      // console.log(this);
+        //     this.addName();
+        // });
+    }
+
+    addName() {
+        console.log(this);
+        const name = new NameField(this.names[this.currentIndex]);
+        this.currentIndex++;
+        if(this.currentIndex >= this.names.length){
+            this.currentIndex = 0;
+        }
+
+    }
+}
+
+const gen = new NameGenerator();
